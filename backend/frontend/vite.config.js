439c2f72f20405da2127998f1339a0f57e0df7bb
Smharis05 <mharis000005@gmail.com>
2025-12-10 10:04:47 +0530

    Updated Code

--- backend/frontend/vite.config.js
+++ backend/frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3001,
     proxy: {
       "/api": {
-        target: "http://localhost:4002",
+        target: "https://chat-application-acdj.onrender.com",
         changeOrigin: true,
       },
 
