8e129fe35066ae6608356b3ac54ad8f859c9d340
Smharis05 <mharis000005@gmail.com>
2025-12-10 14:17:39 +0530

    Updated Code

--- backend/frontend/src/App.jsx
+++ backend/frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 // Components:
-import Left from "./home/leftPart/Left"; // इसे अब Sidebar (Chat List) मानेंगे
-import Right from "./home/rigthPart/Right"; // इसे अब Main Content (Chat Area) मानेंगे
+import Left from "./home/leftPart/Left"; 
+import Right from "./home/rigthPart/Right";
 
 import SignUp from "./components/SignUp";
 import Login from "./components/Login";
@@ -12,7 +12,6 @@ import { Navigate, Route, Routes } from "react-router-dom";
 
 function App() {
   const [authUser] = useAuth();
-  console.log(authUser);
 
   return (
     <>
@@ -21,47 +20,41 @@ function App() {
           path="/"
           element={
             authUser ? (
-              <div className="drawer lg:drawer-open">
-                {/* Checkbox for drawer toggle on small screens */}
-                <input
-                  id="my-drawer-2"
-                  type="checkbox"
-                  className="drawer-toggle"
-                />
+              <div className="drawer lg:drawer-open min-h-screen bg-black">
+                <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
 
-                {/* Right Component (Main Chat Area / Content) */}
-                <div className="drawer-content flex flex-col items-center justify-center">
-                  <Right /> 
+                {/* Right -> MAIN CHAT AREA */}
+                <div className="drawer-content flex flex-col">
+                  <Right />
                 </div>
 
-                {/* Left Component (Sidebar / Chat List) */}
+                {/* Left -> SIDEBAR */}
                 <div className="drawer-side">
-                  <label
-                    htmlFor="my-drawer-2"
-                    aria-label="close sidebar"
-                    className="drawer-overlay"
-                  ></label>
-                  {/* Left component */}
-                  <ul className="menu w-80 min-h-full bg-black text-base-content">
-                    <Left /> 
-                  </ul>
+                  <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+
+                  {/* UL HATAO, DIRECT COMPONENT DO */}
+                  <div className="w-80 min-h-full bg-black text-white">
+                    <Left />
+                  </div>
                 </div>
               </div>
             ) : (
-              // If not logged in, navigate to login
               <Navigate to={"/login"} />
             )
           }
         />
+
         <Route
           path="/login"
           element={authUser ? <Navigate to="/" /> : <Login />}
         />
+
         <Route
           path="/signup"
           element={authUser ? <Navigate to="/" /> : <SignUp />}
         />
       </Routes>
+
       <Toaster />
     </>
   );
