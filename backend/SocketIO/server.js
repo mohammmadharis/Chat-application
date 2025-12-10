1fd7de2475f8404eea22fef80e89b18011ec0e78
Smharis05 <mharis000005@gmail.com>
2025-12-10 10:41:37 +0530

    Updated Code

--- backend/SocketIO/server.js
+++ backend/SocketIO/server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: true,
+    origin: "https://chat-application-acdj.onrender.com",
     methods: ["GET", "POST"],
   },
 });
